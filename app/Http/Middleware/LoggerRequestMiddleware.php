e98e682b33992fe9477669069ae0352e80dddca1
joao bittencourt <joaogsbittencourt@gmail.com>
2024-07-20 13:43:18 -0300

    Em desenv

---
+++ app/Http/Middleware/LoggerRequestMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoggerRequestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        
+        Log::channel('requestlog')->info('requestlog', [
+            'request' => $request->getContent(),
+            'response' => $response->getStatusCode(),
+            'created_at' => Carbon::now(),
+        ]);
+
+        return $response;
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->id == 1;
+        });
+
         Paginator::useBootstrap();
+
     }
 }
